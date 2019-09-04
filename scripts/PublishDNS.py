7b47aa33ded2f232b8f2b1e81a3241115d7b68ff
Ben Tucker <ben_tucker@hotmail.com>
2019-09-04 20:26:36 +1000

    PublishDNS.py - updates and fixes for python3 and boto3.  It should 1/4 work now...

--- scripts/PublishDNS.py
+++ scripts/PublishDNS.py
@@ -8,14 +8,9 @@
 # examples
 # ./PublishDNS.py --AWSRegion ap-southeast-2 --dnsname bentest.benno.ninja.com.au --stackname ben-test-v1
 
-
-from boto import cloudformation
-from boto import config
-from boto import route53
-from boto.ec2 import elb
 from datetime import datetime
 import argparse
-import boto
+import boto3
 import datetime
 import os
 import re
@@ -25,22 +20,22 @@ import time
 import time
 
 # Allow
-ALLOWEDDNSZONES = "benno.ninja.com.au,int.benno.ninja.com.au"
+allowed_dns_zones = "benno.ninja.com.au,int.benno.ninja.com.au"
 
 # How to wait(secs) for the dnscnametarget DNS to become available(in local DNS)
-WAITFORELBDNSSECS = 300
+how_long_to_wait_for_dns = 300
 
 # globals
-BCONNECT53 = None
-BCONNECTstack = None
-BCONNECTec2 = None
-ZONEID = ""
-LIVEDNSREC = None
-dnssuffix = ""
-dnscnametarget = ""
-PASSIVEPRINTELBDNS = True
-
-DEBUG = True
+aws_region = None
+boto_cfn   = None
+boto_elb   = None
+boto_r53   = None
+debug      = False
+dnscnametarget  = ""
+dnssuffix       = ""
+live_dns_record = None
+print_elb_dns   = True
+stackname  = ""
 
 # Pretty Colours
 PINK = '\033[95m'
@@ -71,7 +66,7 @@ def warning(message):
 
 
 def info(message):
-    print(message.replace('<message text>', message)
+    print(message.replace('<message text>', message))
 
 
 def progress(message):
@@ -79,18 +74,18 @@ def progress(message):
 
 
 def debug(message):
-    if DEBUG:
+    if debug:
         print(GREEN + 'debug::' + str(message) + ENDC)
 
 
 def parsecommandline():
     global parser
     global args
-    global AWSREGION
+    global aws_region
     global stackname
     global dnsname
-    global DEBUG
-    global PASSIVEPRINTELBDNS
+    global debug
+    global print_elb_dns
 
     parser = argparse.ArgumentParser(description = 'Utilitiy to add/update an AWS Route53 CNAMEs to a Stacks ELB')
     parser.add_argument('--AWSRegion', default = 'ap-southeast-2', help = "AWS Region")
@@ -101,11 +96,11 @@ def parsecommandline():
 
     args = parser.parse_args()
 
-    AWSREGION = args.AWSRegion
-    stackname = args.stackname
-    dnsname = args.dnsname
-    DEBUG = args.Debug
-    PASSIVEPRINTELBDNS = args.getelbdnsname
+    aws_region    = args.AWSRegion
+    stackname     = args.stackname
+    dnsname       = args.dnsname
+    debug         = args.Debug
+    print_elb_dns = args.getelbdnsname
 
 def RunCommandFore(ltorun):
     try:
@@ -131,28 +126,28 @@ def RunCommandFore(ltorun):
         bail(str(ltorun) + " : failed to run, triggering an exception: ")
 
 def botoconnects():
-    global AWSREGION
-    global BCONNECT53
-    global BCONNECTstack
-    global BCONNECTec2
+    global aws_region
+    global boto_cfn
+    global boto_elb
+    global boto_r53
 
-    BCONNECT53      = boto.route53.connect_to_region(AWSREGION)
-    BCONNECTstack   = boto.cloudformation.connect_to_region(AWSREGION)
-    BCONNECTec2     = boto.ec2.elb.connect_to_region(AWSREGION)
+    boto_cfn = boto3.client('cloudformation',region_name=aws_region)
+    boto_elb = boto3.client('elb',region_name=aws_region)
+    boto_r53 = boto3.client('route53',region_name=aws_region)
 
-    for con in BCONNECT53, BCONNECTstack, BCONNECTec2:
+    for con in boto_cfn, boto_elb, boto_r53:
         if con is None:
-            bail("A boto connect failed","Boto connect failed connecting to AWS Region:" + str(AWSREGION) + " failed")
+            bail("A boto connect failed","Boto connect failed connecting to AWS Region:" + str(aws_region) + " failed")
 
     return True
 
 # Give me a domain name, I'll respond with the AWS HostedZoneID
 def GetR53ZoneID(ldomain):
-    global BCONNECT53
+    global boto_r53
     if ldomain[-1] !=  '.':
         ldomain +=  '.'
 
-    for i in BCONNECT53.get_all_hosted_zones(start_marker = None, zone_list = None)['ListHostedZonesResponse']['HostedZones']:
+    for i in boto_r53.get_all_hosted_zones(start_marker = None, zone_list = None)['ListHostedZonesResponse']['HostedZones']:
         if i['Name'] ==  ldomain:
             return i['Id'].replace('/hostedzone/', '')
 
@@ -160,16 +155,16 @@ def GetR53ZoneID(ldomain):
 # kudos to https://chromium.googlesource.com/external/boto/+/cd1aa815051534a5371817e94dba4c03bb5488f1/bin/route53
 # construct and send back a DNSCNameRecord Object
 def GetR53CRecord(ldnsrec):
-    global BCONNECT53
-    global ZONEID
+    global boto_r53
+    global zone_id
 
     lname = ldnsrec.name + '.' #+ dnssuffix + '.'
 
-    res = BCONNECT53.get_all_rrsets(ZONEID, type = "CNAME", name = lname)
+    res = boto_r53.get_all_rrsets(zone_id, type = "CNAME", name = lname)
     for record in res:
         #print '%-40s %-5s %-20s %s' %(record.name, record.type, record.ttl, record.to_print())
         if record.name == lname:
-            ldnsrec.zoneid = ZONEID
+            ldnsrec.zoneid = zone_id
             ldnsrec.dnscnametarget = record.to_print()
             ldnsrec.ttl = record.ttl
             ldnsrec.orignalttl = record.ttl
@@ -177,25 +172,22 @@ def GetR53CRecord(ldnsrec):
     # not found return -1
     return(-1)
 
-def StackStatus(lstack_name):
-    global BCONNECTstack
+
+def StackStatus(stack_name):
+    global aws_region
+    global boto_cfn
+
     try:
-        stk = BCONNECTstack.describe_stacks(lstack_name)
-        # return first hit
-        for i in stk:
-            return i.stack_status
-    except boto.exception.BotoServerError:
-        # parse response  = > its *probably* stack not found
-        # <Message>Stack:lsredis-dev does not exist</Message>
-        es = str(sys.exc_info()).replace("\n","")
+        stk = boto_cfn.describe_stacks(StackName=stack_name)
+        if stk['Stacks'][-1]['StackName'] == stack_name:
+            return stk['Stacks'][-1]['StackStatus']
 
-        m1 = re.search('(^.+)(<Message>)(.+)(</Message>)(.+)', es.replace("\n",""))
-        if m1:
-            msg = m1.group(3)
-            if msg.count('Stack') > 0 and msg.count(lstack_name) > 0 and msg.count('does not exist'):
-                return -1
+    except boto_cfn.exceptions.ClientError as e:
+        if e.response['Error']['Message'] == "Stack with id " + stack_name + " does not exist":
+            bail("unable to find stack:" + stack_name + " ,in region:" + aws_region)
 
-        bail("Internal Error","Failed to parse the error output from cloudformation via boto")
+        bail("Internal Error in def StackStatus. Failed to parse the error output from cloudformation via boto")
+
 
 def SetDNSCnameTTL(ldnsobject, lnewttl):
     global dnssuffix
@@ -204,7 +196,7 @@ def SetDNSCnameTTL(ldnsobject, lnewttl):
         ldnsobject.ttl +
         'secs, changing to ' +
         str(lnewttl) + 'secs')
-    ret = BCONNECT53.get_zone(dnssuffix).update_cname(ldnsobject.name + "." + dnssuffix +".", ldnsobject.dnscnametarget, ttl = int(lnewttl), identifier = None, comment = 'was:'+str(ldnsobject.orignalttl) + 'setdown for blue/green deployment')
+    ret = boto_r53.get_zone(dnssuffix).update_cname(ldnsobject.name + "." + dnssuffix +".", ldnsobject.dnscnametarget, ttl = int(lnewttl), identifier = None, comment = 'was:'+str(ldnsobject.orignalttl) + 'setdown for blue/green deployment')
     if 'Status:PENDING' in str(ret):
         return 0
     else:
@@ -216,7 +208,7 @@ def SetDNSCNAME(ldnsobject, lnewcname):
     dnscnametarget = lnewcname + '.'
     info('updating DNS CNAME:' + record + ' to point to:' + dnscnametarget)
     # we keep the DNS at min value
-    ret = BCONNECT53.get_zone(dnssuffix).update_cname(record, dnscnametarget, ttl = 60, identifier = None, comment = 'PublishDNS.py')
+    ret = boto_r53.get_zone(dnssuffix).update_cname(record, dnscnametarget, ttl = 60, identifier = None, comment = 'PublishDNS.py')
     if 'Status:PENDING' in str(ret):
         # update object & safety sleep
         ldnsobject.dnscnametarget = dnscnametarget
@@ -234,7 +226,7 @@ def AddDNSCNAME(ldnsobject, lnewcname):
     info('Adding DNS CNAME:' + record + ' pointing to :' + dnscnametarget)
 
     # Todo: Not hardcode the DNS(min) value
-    ret = BCONNECT53.get_zone(dnssuffix).add_cname(record, dnscnametarget, ttl = 60, identifier = None, comment = 'PublishDNS.py')
+    ret = boto_r53.get_zone(dnssuffix).add_cname(record, dnscnametarget, ttl = 60, identifier = None, comment = 'PublishDNS.py')
     if 'Status:PENDING' in str(ret):
         ldnsobject.dnscnametarget = dnscnametarget
         time.sleep(5)
@@ -274,90 +266,96 @@ def PollForCNAMESwitch(lhost, lmatch, lalarm ):
     return -1
 
 def parseDNSSuffix(ldnsname):
-    global ALLOWEDDNSZONES
+    global allowed_dns_zones
 
-    for anallowed in reversed(sorted(ALLOWEDDNSZONES.split(','), key = len) ):
+    for anallowed in reversed(sorted(allowed_dns_zones.split(','), key = len) ):
         if anallowed in ldnsname:
             debug('matched(allowed) DNS zone: ' + str(anallowed))
             return anallowed
     return -1
 
-# Todo, what if there is more than one?
-def GetFirstELBFromStack(lstack_name):
-    global BCONNECTstack
-    sr = BCONNECTstack.list_stack_resources(lstack_name)
-    for i in sr:
-        if i.resource_type ==  'AWS::ElasticLoadBalancing::LoadBalancer':
-            return i.physical_resource_id
+
+def GetFirstELBFromStack(stack_name):
+    global boto_cfn
+    sr = boto_cfn.list_stack_resources(StackName=stack_name)
+
+    for i in sr['StackResourceSummaries']:
+        if i['LogicalResourceId'] == "LoadBalancer":
+            return i['PhysicalResourceId']
 
     return -1
 
+
 def GetELBDNSName(lelbname):
-    global BCONNECTec2
-    return(BCONNECTec2.get_all_load_balancers(lelbname)[0].dns_name)
+    global boto_elb
+    return boto_elb.describe_load_balancers(LoadBalancerNames=[lelbname])['LoadBalancerDescriptions'][-1]['DNSName']
 
 
-dnscnametarget = -1
-parsecommandline()
-botoconnects()
+def main():
+    dnscnametarget = -1
+    parsecommandline()
+    botoconnects()
 
-ret = StackStatus(stackname)
-if ret ==  -1:
-    bail('stack not found, cannot find stack with name:' + str(stackname) )
-if 'COMPLETE' not in ret :
-    bail('stack not in COMPLETE state, stack(' + str(stackname) + ') is not in a COMPLETE state(' + str(ret) + ')' )
+    ret = StackStatus(stackname)
+    if ret ==  -1:
+        bail('stack not found, cannot find stack with name:' + str(stackname) )
+    if 'COMPLETE' not in ret :
+        bail('stack not in COMPLETE state, stack(' + str(stackname) + ') is not in a COMPLETE state(' + str(ret) + ')' )
 
-elb = GetFirstELBFromStack(stackname)
-if elb ==  -1:
-    bail('ELB not found, cannot find ELB for stack(' + stackname)
-else:
-    info('Stack(' + stackname + '), found ELB:' + elb)
-    dnscnametarget = GetELBDNSName(elb)
+    elb = GetFirstELBFromStack(stackname)
+    if elb ==  -1:
+        bail('ELB not found, cannot find ELB for stack(' + stackname)
+    else:
+        info('Stack(' + stackname + '), found ELB:' + elb)
+        dnscnametarget = GetELBDNSName(elb)
 
-if dnscnametarget ==  -1:
-    bail('internal error, the DNS dnscnametarget is invalid')
-if len(dnscnametarget) < 20:    # todo: don't hardcode this
-    bail('internal error, the DNS dnscnametarget is not sane(too short - under 20chars)')
+    if dnscnametarget ==  -1:
+        bail('internal error, the DNS dnscnametarget is invalid')
+    if len(dnscnametarget) < 20:    # todo: don't hardcode this
+        bail('internal error, the DNS dnscnametarget is not sane(too short - under 20chars)')
 
-# if this is a very new stack, the ELB name might not yet resolve, so poll...
-ret = PollForHostnameResolve(dnscnametarget, WAITFORELBDNSSECS)
-if ret ==  -1:
-    bail('Timeout on resolution of the ELB DNS name. ' + str(dnscnametarget) + ' does not resolve')
+    # if this is a very new stack, the ELB name might not yet resolve, so poll...
+    ret = PollForHostnameResolve(dnscnametarget, how_long_to_wait_for_dns)
+    if ret ==  -1:
+        bail('Timeout on resolution of the ELB DNS name. ' + str(dnscnametarget) + ' does not resolve')
 
-# just print elb dns name and exit...
-if PASSIVEPRINTELBDNS ==  True:
-    print(dnscnametarget)
-    sys.exit(0)
+    # just print elb dns name and exit...
+    if print_elb_dns ==  True:
+        print(dnscnametarget)
+        sys.exit(0)
 
-ret = parseDNSSuffix(dnsname)
-if ret ==  -1:
-    bail('invalid DNSSuffix', 'Parsing(' + str(dnsname) + ') couldnt find in an ALLOWED DNS Suffixs(' + str(ALLOWEDDNSZONES) + ')')
-else:
-    dnssuffix = ret
+    ret = parseDNSSuffix(dnsname)
+    if ret ==  -1:
+        bail('invalid DNSSuffix', 'Parsing(' + str(dnsname) + ') couldnt find in an ALLOWED DNS Suffixs(' + str(allowed_dns_zones) + ')')
+    else:
+        dnssuffix = ret
 
-ZONEID = GetR53ZoneID(dnssuffix + '.')
-info('Using AWS zoneid:'+ZONEID + ' for ' + dnssuffix)
+    zone_id = GetR53ZoneID(dnssuffix + '.')
+    info('Using AWS zoneid:'+zone_id + ' for ' + dnssuffix)
 
-# does the dnsname already exist? if yes record it, then switch it
-LIVEDNSREC = DNSCNameRecord(dnsname)
-ret = GetR53CRecord(LIVEDNSREC)
-if ret ==  0:
-    info(str(dnsname) + ' already exists, dig follows...')
-    info(str(RunCommandFore('dig ' + dnsname) ))
-    info('TTL = ' +str(LIVEDNSREC.ttl) + ' seconds')
-    info('-----------------------------------------------------------------------------')
-    ret =  SetDNSCNAME(LIVEDNSREC, dnscnametarget)
-else:
-    ret =  AddDNSCNAME(LIVEDNSREC, dnscnametarget)
+    # does the dnsname already exist? if yes record it, then switch it
+    live_dns_record = DNSCNameRecord(dnsname)
+    ret = GetR53CRecord(live_dns_record)
+    if ret ==  0:
+        info(str(dnsname) + ' already exists, dig follows...')
+        info(str(RunCommandFore('dig ' + dnsname) ))
+        info('TTL = ' +str(live_dns_record.ttl) + ' seconds')
+        info('-----------------------------------------------------------------------------')
+        ret =  SetDNSCNAME(live_dns_record, dnscnametarget)
+    else:
+        ret =  AddDNSCNAME(live_dns_record, dnscnametarget)
 
-if ret !=  int(0):
-    warning('warning: Route53 DNS CNAME add/update returned an error and may have failed! Will continue to polling for result')
+    if ret !=  int(0):
+        warning('warning: Route53 DNS CNAME add/update returned an error and may have failed! Will continue to polling for result')
 
-if LIVEDNSREC.ttl ==  None:
-    LIVEDNSREC.ttl = 60
+    if live_dns_record.ttl ==  None:
+        live_dns_record.ttl = 60
 
-ret =  PollForCNAMESwitch(LIVEDNSREC.name + '.' , dnscnametarget ,(LIVEDNSREC.ttl*2))
-if ret ==  -1:
-    bail('DNS CNAME update fail, the DNS CNAME update has not propergated')
+    ret =  PollForCNAMESwitch(live_dns_record.name + '.' , dnscnametarget ,(live_dns_record.ttl*2))
+    if ret ==  -1:
+        bail('DNS CNAME update fail, the DNS CNAME update has not propergated')
 
-info(LIVEDNSREC.name + ' -> ELB(stack = ' + stackname + ')' )
+    info(live_dns_record.name + ' -> ELB(stack = ' + stackname + ')' )
+
+if __name__ == "__main__":
+    main()
